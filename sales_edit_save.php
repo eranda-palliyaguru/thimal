ac0de4e6b000c334bb9cd9254f9ee72722803806
eranda-palliyaguru <74122774+eranda-palliyaguru@users.noreply.github.com>
2020-11-15 07:37:02 +0530

    update sales edit

--- sales_edit_save.php
+++ sales_edit_save.php
@@ -16,6 +16,7 @@ $result = $db->prepare("SELECT * FROM sales WHERE invoice_number='$invo' ");
 $cus=$row['name'];
 $cus_id=$row['customer_id'];
 $sales_id=$row['transaction_id'];
+$loading_id=$row['loading_id'];
 
 		}
 
@@ -27,46 +28,46 @@ $pro_id=$row['product_id'];
 $name=$row['gen_name'];
 $price=$row['price'];
 $o_price=$row['o_price'];
-	
 
-			
+
+
 $qty=$_POST[$pro_id];
 $amount=$price*$qty;
 $profit=$price-$o_price;
 $profit=$profit*$qty;
 $date=date("Y-m-d");
-			
-$list_id="0";			
+
+$list_id="0";
 $result1 = $db->prepare("SELECT * FROM sales_list WHERE invoice_no='$invo' and product_id='$pro_id'  ");
 		$result1->bindParam(':userid', $res);
 		$result1->execute();
 		for($i=0; $row1 = $result1->fetch(); $i++){
 		$list_id=$row1['id'];
-		}			
-			
-			
+		}
+
+
 if($list_id>1){
-	
-$sql = "UPDATE sales_list 
+
+$sql = "UPDATE sales_list
         SET qty=?,amount=?,profit=?
 		WHERE id=?";
 $q = $db->prepare($sql);
 $q->execute(array($qty,$amount,$profit,$list_id));
-	
+
 if($qty==""){
-	
+
 	$result = $db->prepare("DELETE FROM sales_list WHERE  id= :memid");
 	$result->bindParam(':memid', $list_id);
 	$result->execute();
-}	
-	
-	
-}else{ 
-	
+}
+
+
+}else{
+
 if($qty>0){
-$sql = "INSERT INTO sales_list (product_id,name,qty,price,amount,profit,date,invoice_no) VALUES (:a,:b,:c,:d,:e,:f,:g,:invo)";
+$sql = "INSERT INTO sales_list (product_id,name,qty,price,amount,profit,date,action,invoice_no,loading_id,cus_id) VALUES (:a,:b,:c,:d,:e,:f,:g,:act,:invo,:load,:cus)";
 $ql = $db->prepare($sql);
-$ql->execute(array(':a'=>$pro_id,':b'=>$name,':c'=>$qty,':d'=>$price,':e'=>$amount,':f'=>$profit,':g'=>$date,':invo'=>$invo));
+$ql->execute(array(':a'=>$pro_id,':b'=>$name,':c'=>$qty,':d'=>$price,':e'=>$amount,':f'=>$profit,':g'=>$date,':act'=>'3',':invo'=>$invo,':load'=>$loading_id,':cus'=>$cus_id));
 } }
 		}
 
@@ -88,7 +89,7 @@ $result1 = $db->prepare("SELECT sum(profit) FROM sales_list WHERE invoice_no='$i
 		$tot_profit=$row1['sum(profit)'];
 		}
 
-$sql = "UPDATE sales 
+$sql = "UPDATE sales
         SET amount=?,balance=?,profit=?
 		WHERE transaction_id=?";
 $q = $db->prepare($sql);
