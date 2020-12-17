2aad3dd441cea8bba2490844ab769f384988b211
eranda <erandasampath2000@gmail.com>
2020-12-17 20:44:58 +0530

    up

---
+++ lorry_credit_pay.php
@@ -0,0 +1,276 @@
+<!DOCTYPE html>
+<html>
+<?php
+include("head.php");
+include("connect.php");
+?>
+<body class="hold-transition skin-blue layout-top-nav">
+<?php
+include_once("auth.php");
+$r=$_SESSION['SESS_LAST_NAME'];
+
+if($r =='Cashier'){
+
+include_once("sidebar2.php");
+}
+if($r =='admin'){
+
+//include_once("sidebar.php");
+}
+?>
+
+
+
+
+<link rel="stylesheet" href="datepicker.css"
+        type="text/css" media="all" />
+    <script src="datepicker.js" type="text/javascript"></script>
+    <script src="datepicker.ui.min.js"
+        type="text/javascript"></script>
+ <script type="text/javascript">
+
+		 $(function(){
+        $("#datepicker1").datepicker({ dateFormat: 'yy/mm/dd' });
+        $("#datepicker2").datepicker({ dateFormat: 'yy/mm/dd' });
+
+    });
+
+    </script>
+    <!-- /.sidebar -->
+  </aside>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        #SUM
+        <small>Preview</small>
+      </h1>
+    </section>
+	<form >
+	 <select class="form-control select2" name="cus" style="width: 350px;"  autofocus >
+
+
+				  <?php
+                $result = $db->prepare("SELECT * FROM customer ");
+		$result->bindParam(':userid', $res);
+		$result->execute();
+		for($i=0; $row = $result->fetch(); $i++){
+	?>
+		<option value="<?php echo $row['customer_id'];?>"><?php echo $row['customer_name']; ?>    </option>
+	<?php
+				}
+			?>
+                </select>
+		 <button class="btn btn-info" style="width: 123px; height:35px; margin-top:-8px;margin-left:8px;" type="submit">
+ <i class="icon icon-search icon-large"></i> Search
+ </button>
+  </form>
+     <div class="row">
+      <div id="screen"></div>
+
+<div class="col-md-6">
+              <div class="form-group"><br>
+
+                <label>#SUM</label>
+
+				</div>
+
+
+	<table id="example1" class="table table-bordered table-striped">
+
+                <thead>
+                <tr>
+
+                  <th>Name</th>
+				  <th>Invoice no</th>
+                  <th>Date</th>
+                  <th>Amount</th>
+
+                </tr>
+
+                </thead>
+
+                <tbody>
+				<?php
+   $cus=$_GET['cus'];
+	$tot=0;
+	$result2z = $db->prepare("SELECT * FROM payment WHERE action='2' and type='credit' and customer_id='$cus'");
+  $result2z->bindParam(':userid', $d2);
+  $result2z->execute();
+  for($i=0; $row = $result2z->fetch(); $i++){
+				$sales_id=$row['sales_id'];
+
+		$result2 = $db->prepare("SELECT * FROM sales WHERE action='1' AND transaction_id='$sales_id'");
+			    $result2->bindParam(':userid', $d2);
+                $result2->execute();
+                for($i=0; $row2 = $result2->fetch(); $i++){
+				$invo=$row2['invoice_number'];
+
+		 $pay_type=$row['type'];
+		$action=$row['action'];
+
+
+		    $date1=$row2['date'];
+			$date =  date("Y-m-d");
+				  $sday= strtotime( $date1);
+                  $nday= strtotime($date);
+                  $tdf= abs($nday-$sday);
+                  $nbday1= $tdf/86400;
+                  $rs1= intval($nbday1);
+
+
+
+			?>
+               <tr class="record" >
+
+				<td><?php echo $row2['name'];?></td>
+				<td><?php echo $row2['transaction_id'];?></td>
+				<td><?php echo $row2['date'];?></td>
+				<td><?php echo $row['amount']-$row['pay_amount'];
+					$tot+=$row['amount']-$row['pay_amount'];?></td>
+
+
+				   <?php
+				}
+				}
+				?>
+                </tr>
+
+                </tbody>
+                <tfoot>
+                </tfoot>
+              </table>
+
+
+
+			<h2 style="color: red">Credit - Rs.<?php echo $tot; ?></h2>
+
+
+
+              </div><br><br><br>
+        <!-- /.col -->
+	<div class="col-md-2"><a href="sales_start.php">
+	<button type="button" class="btn btn-block btn-success btn-sm">Home</button></a>  </div><br>
+        <!-- /.col -->
+      </div>
+    <!-- Main content -->
+
+
+
+    <!-- /.content -->
+  </div>
+
+  <!-- /.content-wrapper -->
+    <?php
+ // include("dounbr.php");
+?>
+  <!-- /.control-sidebar -->
+  <!-- Add the sidebar's background. This div must be placed
+       immediately after the control sidebar -->
+  <div class="control-sidebar-bg"></div>
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery 2.2.3 -->
+<script src="../../plugins/jQuery/jquery-2.2.3.min.js"></script>
+<!-- Bootstrap 3.3.6 -->
+<script src="../../bootstrap/js/bootstrap.min.js"></script>
+<!-- Select2 -->
+<script src="../../plugins/select2/select2.full.min.js"></script>
+<!-- InputMask -->
+<script src="../../plugins/input-mask/jquery.inputmask.js"></script>
+<script src="../../plugins/input-mask/jquery.inputmask.date.extensions.js"></script>
+<script src="../../plugins/input-mask/jquery.inputmask.extensions.js"></script>
+<!-- date-range-picker -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js"></script>
+<script src="../../plugins/daterangepicker/daterangepicker.js"></script>
+<!-- bootstrap datepicker -->
+<script src="../../plugins/datepicker/bootstrap-datepicker.js"></script>
+<!-- bootstrap color picker -->
+<script src="../../plugins/colorpicker/bootstrap-colorpicker.min.js"></script>
+<!-- bootstrap time picker -->
+<script src="../../plugins/timepicker/bootstrap-timepicker.min.js"></script>
+<!-- SlimScroll 1.3.0 -->
+<script src="../../plugins/slimScroll/jquery.slimscroll.min.js"></script>
+<!-- iCheck 1.0.1 -->
+<script src="../../plugins/iCheck/icheck.min.js"></script>
+<!-- FastClick -->
+<script src="../../plugins/fastclick/fastclick.js"></script>
+<!-- AdminLTE App -->
+<script src="../../dist/js/app.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="../../dist/js/demo.js"></script>
+<!-- Page script -->
+<script>
+  $(function () {
+    //Initialize Select2 Elements
+    $(".select2").select2();
+
+    //Datemask dd/mm/yyyy
+    $("#datemask").inputmask("YYYY/MM/DD", {"placeholder": "YYYY/MM/DD"});
+    //Datemask2 mm/dd/yyyy
+    $("#datemask2").inputmask("YYYY/MM/DD", {"placeholder": "YYYY/MM/DD"});
+    //Money Euro
+    $("[data-mask]").inputmask();
+
+    //Date range picker
+    $('#reservation').daterangepicker();
+    //Date range picker with time picker
+    $('#reservationtime').daterangepicker({timePicker: true, timePickerIncrement: 30, format: 'YYYY/MM/DD h:mm A'});
+    //Date range as a button
+    $('#daterange-btn').daterangepicker(
+        {
+          ranges: {
+            'Today': [moment(), moment()],
+            'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+            'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+            'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+            'This Month': [moment().startOf('month'), moment().endOf('month')],
+            'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+          },
+          startDate: moment().subtract(29, 'days'),
+          endDate: moment()
+        },
+        function (start, end) {
+          $('#daterange-btn span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+        }
+    );
+
+    //Date picker
+	$('#datepicker').datepicker({  autoclose: true, datepicker: true,  format: 'yyyy/mm/dd '});
+    $('#datepicker').datepicker({
+      autoclose: true
+    });
+
+    //iCheck for checkbox and radio inputs
+    $('input[type="checkbox"].minimal, input[type="radio"].minimal').iCheck({
+      checkboxClass: 'icheckbox_minimal-blue',
+      radioClass: 'iradio_minimal-blue'
+    });
+    //Red color scheme for iCheck
+    $('input[type="checkbox"].minimal-red, input[type="radio"].minimal-red').iCheck({
+      checkboxClass: 'icheckbox_minimal-red',
+      radioClass: 'iradio_minimal-red'
+    });
+    //Flat red color scheme for iCheck
+    $('input[type="checkbox"].flat-red, input[type="radio"].flat-red').iCheck({
+      checkboxClass: 'icheckbox_flat-green',
+      radioClass: 'iradio_flat-green'
+    });
+
+    //Colorpicker
+    $(".my-colorpicker1").colorpicker();
+    //color picker with addon
+    $(".my-colorpicker2").colorpicker();
+
+    //Timepicker
+    $(".timepicker").timepicker({
+      showInputs: false
+    });
+  });
+
+</script>
+</body>
+</html>
