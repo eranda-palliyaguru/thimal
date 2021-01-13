dee5390c258725571513d4ffd06887ce48f55467
eranda-plliyaguu <erandasampath2000@gmil.com>
2021-01-13 15:55:55 +0530

    up

--- sales_credit.php
+++ sales_credit.php
@@ -41,7 +41,9 @@ include_once("sidebar2.php");
     });
 
     </script>
-
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+      <script> window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
+      <script src="js/main.js"></script>
 
 
     <div id="loader-wrapper">
