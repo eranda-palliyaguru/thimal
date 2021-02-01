a12aea0913672edf53e904f17e92ddc5a54d8ae9
eranda-palliyaguru <erandasampath2000@gmali.com>
2021-02-01 18:52:24 +0530

    special price error fix

--- sales_rp_special.php
+++ sales_rp_special.php
@@ -44,7 +44,7 @@ $q->execute(array($hs));
 
 
 $fix_12='1443';
-$fix_37="4866";
+$fix_37="5950";
 $fix_5="575";
 
 $result1 = $db->prepare("SELECT * FROM special_price WHERE product_id='3' AND price < '$fix_37' ");
