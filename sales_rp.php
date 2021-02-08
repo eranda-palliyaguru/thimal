d64a96a917475b3ee34b40a355c4e11c236fe143
eranda-palliyaguru <erandasampath2000@gmali.com>
2021-02-08 22:30:40 +0530

    up

--- sales_rp.php
+++ sales_rp.php
@@ -386,17 +386,15 @@ if($cus_t > 0){
 			?></span></td>
 					<?php } ?>
 <?php
-$result = $db->prepare("SELECT count(product_id) FROM products WHERE product_id >'9' ");
+$result111212 = $db->prepare("SELECT * FROM products WHERE product_id >'9' ");
 
-    $result->bindParam(':userid', $d1);
-          $result->execute();
-          for($i=0; $row = $result->fetch(); $i++){
-$ter1= $row['count(product_id)'];
-  }
+    $result111212->bindParam(':userid', $d1);
+          $result111212->execute();
+          for($i=0; $row111212 = $result111212->fetch(); $i++){
+$pro_id= $row111212['product_id'];
 
 
-				for($pro_id2 = 0; $pro_id2 < (int)$ter1; $pro_id2++) {
-	            $pro_id=$pro_id2+10;
+
 
 			?>
 
@@ -504,16 +502,14 @@ $tot_f+=$row2['profit'];
 
 					<?php } ?>
 <?php
-$result = $db->prepare("SELECT count(product_id) FROM products WHERE product_id >'9' ");
-    $result->bindParam(':userid', $d1);
-          $result->execute();
-          for($i=0; $row = $result->fetch(); $i++){
-$ter1= $row['count(product_id)'];
-  }
+$result111212 = $db->prepare("SELECT * FROM products WHERE product_id >'9' ");
 
-				for($pro_id2 = 0; $pro_id2 < (int)$ter1; $pro_id2++) {
-	            $pro_id=$pro_id2+10;
+    $result111212->bindParam(':userid', $d1);
+          $result111212->execute();
+          for($i=0; $row111212 = $result111212->fetch(); $i++){
+$pro_id= $row111212['product_id'];
 
+ 
 			?>
 
 
