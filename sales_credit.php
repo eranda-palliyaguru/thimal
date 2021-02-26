f439e1195df2671510378390b7ec6729efaba12d
eranda-palliyaguru <erandasampath2000@gmail.com>
2021-02-26 11:53:37 +0530

    up

--- sales_credit.php
+++ sales_credit.php
@@ -217,168 +217,175 @@ include_once("sidebar2.php");
 
             <div class="box-body">
 	       <table id="example2" class="table table-bordered table-striped">
-      
-                   <thead>
+                <thead>
+                <tr>
+                  <th colspan="4" ></th>
+				    <th colspan="5" >#</th>
+                </tr>
+
+				<tr>
+				<th>Cus_id</th>
+				<th>Customer</th>
+				<th>Invoice</th>
+				<th>Date</th>
 
 
-          <tr>
-          <th>id</th>
-          <th style="width:30%;">Customer</th>
-          <th>Invoice</th>
-          <th>Date</th>
+				<th>Limit</th>
+				<th>Amount</th>
+				<th>Overdue</th>
+				<th>Memo</th>
+				<th>#</th>
+				</tr>
+				</thead>
+
+                <tbody>
+				<?php
+					$tot=0;
+	    date_default_timezone_set("Asia/Colombo");
+		$hh=date("Y/m/d");
+		$pay_type="";
+
+    $type=$_GET['type'];
+    $customer_id=$_GET['cus'];
+     $group=$_GET['group'];
+     $lorry=$_GET['lorry'];
+     $customer_type=$_GET['customer_type'];
+
+     if($customer_id=="all"){
+     if($group=="all"){
+
+     if ($customer_type=="all") {
+     $customer = $db->prepare("SELECT * FROM customer  ORDER BY category DESC");
+     }else {
+     $customer = $db->prepare("SELECT * FROM customer WHERE type='$customer_type' ORDER BY category DESC");
+     }
+
+     }else {
+     $customer = $db->prepare("SELECT * FROM customer WHERE category='$group' ORDER BY category DESC");
+     }
+       		}else{
+     	$customer = $db->prepare("SELECT * FROM customer WHERE customer_id='$customer_id' ORDER BY category DESC "); }
+
+   $customer->bindParam(':userid', $d2);
+   $customer->execute();
+   for($i=0; $row_cus = $customer->fetch(); $i++){
+    $cus=$row_cus['customer_id'];
+     $limit=$row_cus['credit_period'];
 
 
+   $b_tot=0;
+   $pay_tot=0;
+
+   $result2z = $db->prepare("SELECT * FROM payment WHERE action='2' and type='credit' and customer_id='$cus'");
+    $result2z->bindParam(':userid', $d2);
+    $result2z->execute();
+    for($i=0; $row = $result2z->fetch(); $i++){
+    $sales_id=$row['sales_id'];
+
+   if ($lorry=='all') {
+   $result2 = $db->prepare("SELECT * FROM sales WHERE action='1' AND transaction_id='$sales_id'");
+   }else {
+   $result2 = $db->prepare("SELECT * FROM sales WHERE action='1' AND transaction_id='$sales_id' AND lorry_no='$lorry' ");
+   }
+
+      $result2->bindParam(':userid', $d2);
+             $result2->execute();
+             for($i=0; $row2 = $result2->fetch(); $i++){
+    $invo=$row2['invoice_number'];
+
+   $pay_type=$row['type'];
+   $action=$row['action'];
 
 
-          <th style="width:10%;">Credit</th>
-          <th style="width:10%;">payment</th>
-          <th>Limit</th>
-          <th>Overdue</th>
+    $date1=$row2['date'];
+   $date =  date("Y-m-d");
+      $sday= strtotime( $date1);
+               $nday= strtotime($date);
+               $tdf= abs($nday-$sday);
+               $nbday1= $tdf/86400;
+               $rs1= intval($nbday1);
 
-          </tr>
-          </thead>
-
-                   <tbody>
-          <?php
-            $tot=0;
-         date_default_timezone_set("Asia/Colombo");
-         $hh=date("Y/m/d");
-         $pay_type="";
-
-          //$d3=$_SESSION['SESS_FIRST_NAME'];
-          $type=$_GET['type'];
-          $customer_id=$_GET['cus'];
-           $group=$_GET['group'];
-           $lorry=$_GET['lorry'];
-           $customer_type=$_GET['customer_type'];
-
-           if($customer_id=="all"){
-           if($group=="all"){
-
-           if ($customer_type=="all") {
-           $customer = $db->prepare("SELECT * FROM customer  ORDER BY category DESC");
-           }else {
-           $customer = $db->prepare("SELECT * FROM customer WHERE type='$customer_type' ORDER BY category DESC");
-           }
-
-           }else {
-           $customer = $db->prepare("SELECT * FROM customer WHERE category='$group' ORDER BY category DESC");
-           }
-             		}else{
-           	$customer = $db->prepare("SELECT * FROM customer WHERE customer_id='$customer_id' ORDER BY category DESC "); }
-
-         $customer->bindParam(':userid', $d2);
-         $customer->execute();
-         for($i=0; $row_cus = $customer->fetch(); $i++){
-          $cus=$row_cus['customer_id'];
-           $limit=$row_cus['credit_period'];
-
-
-         $b_tot=0;
-         $pay_tot=0;
-
-         $result2z = $db->prepare("SELECT * FROM payment WHERE action='2' and type='credit' and customer_id='$cus'");
-          $result2z->bindParam(':userid', $d2);
-          $result2z->execute();
-          for($i=0; $row = $result2z->fetch(); $i++){
-          $sales_id=$row['sales_id'];
-
-         if ($lorry=='all') {
-         $result2 = $db->prepare("SELECT * FROM sales WHERE action='1' AND transaction_id='$sales_id'");
-         }else {
-         $result2 = $db->prepare("SELECT * FROM sales WHERE action='1' AND transaction_id='$sales_id' AND lorry_no='$lorry' ");
-         }
-
-            $result2->bindParam(':userid', $d2);
-                   $result2->execute();
-                   for($i=0; $row2 = $result2->fetch(); $i++){
-          $invo=$row2['invoice_number'];
-
-         $pay_type=$row['type'];
-         $action=$row['action'];
-
-
-          $date1=$row2['date'];
-         $date =  date("Y-m-d");
-            $sday= strtotime( $date1);
-                     $nday= strtotime($date);
-                     $tdf= abs($nday-$sday);
-                     $nbday1= $tdf/86400;
-                     $rs1= intval($nbday1);
-
-         if($type=='due'){ $leval=$rs1-$limit;	}else { $leval=$rs1; }
-         $coo=$limit;
-         $rs1=$rs1-$limit;
-
-
-
-         if($leval >= 1){
-         $color="";$color1="";
-         if($rs1>=30){$color="#f0f296"; $color1="black";}
-         if($rs1>=60){$color="#701144"; $color1="white";}
-
-
-            ?>
-           <tr style="background-color: <?php echo $color; ?>; color: <?php echo $color1; ?>">
-          <td ><?php echo $row['customer_id'];?></td>
-          <td><?php echo $cus_name=$row2['name'];?></td>
-          <td><?php echo $row2['transaction_id'];?></td>
-          <td><?php echo $row2['date'];?></td>
-
-
-         <?php
-            $ter1=7;
-         $tot+=$row['amount']-$row['pay_amount'];
-         ?>
-
-
-         <td><?php echo number_format($row['amount']-$row['pay_amount'],1);
-         $b_tot+=$row['amount']-$row['pay_amount']; ?></td>
-         <td><?php if($row['pay_amount']>'0'){	echo number_format($row['pay_amount'],1); $pay_tot+=$row['pay_amount']; } ?></td>
-         <td><?php echo $row_cus['credit_period'];?></td>
-         <td><?php	echo $rs1;	?></td>
-
-            </tr>
-          <?php
-         } }
-          }
-
-         if($b_tot > 1){
-         ?>
-               <tr   class=" bg-gray"   >
-
-
-          <td  colspan="4" ><hr><h4> <?php echo  $cus_name; ?>  ___ Total</h4></td>
-
-
-         <td><hr> <h4><?php echo number_format($b_tot,1); ?></h4> </td>
-         <td><hr><h4><?php echo number_format($pay_tot,1); ?></h4></td>
-
-         <td colspan="2"><hr><h4>.</h4></td>
-                   </tr>
-
-                   <tr >
-                     <td colspan="8">.<hr></td>
-                       </tr>
-         <?php } } ?>
-
-
-         <tr>
-           <td  colspan="3" >Total</td>
+   if($type=='due'){ $leval=$rs1-$limit;	}else { $leval=$rs1; }
+   $coo=$limit;
+   $rs1=$rs1-$limit;
 
 
 
+		if($leval >= 0){
+		$color="";$color1="";
+		if($rs1>=30){$color="#f0f296"; $color1="black";}
+		if($rs1>=60){$color="#701144"; $color1="white";}
+$bank=$row['bank_id'];
+//if ($bank==1) {
+  // code...
 
-          <td></td>
-          <td> <h3><?php echo number_format($tot,1); ?></h3> </td>
-          <td></td>
 
-          <td></td><td></td><td></td>
-         </tr>
-                   </tbody>
+					?>
+        <tr style="background-color: <?php echo $color; ?>; color: <?php echo $color1; ?>">
+				<td ><?php echo $row['customer_id'];?></td>
+				<td><?php echo $row2['name'];?></td>
+				<td><?php echo $row2['transaction_id'];?>
+        <span class="pull-right badge bg-green"><?php echo $row2['lorry_no'];?> </span></td>
+				<td><?php echo $row2['date'];?></td>
 
 
-                 </table>
+<?php
+				  $ter1=7;
+			$tot+=$row['amount']-$row['pay_amount'];
+			?>
+
+
+		<td><?php	echo $row_cus['credit_period'];	?></td>
+		<td><?php echo $row['amount']-$row['pay_amount'];
+			$b_tot+=$row['amount']-$row['pay_amount'];
+			if($row['pay_amount']>'0'){?><span class="pull-right badge bg-black"><?php	echo $row['pay_amount'];?></span><?php } ?></td>
+		<td><?php	echo $rs1;	?></td>
+			<td><?php echo $row['memo'];
+			?></td>
+			<td><?php if($user_lewal < 4){?><a rel="facebox" href="payment_view_view.php?id=<?php echo $row2['transaction_id'];?>&pay_amount=<?php echo $row['pay_amount'];?>&pay_id=<?php echo $row['transaction_id'];?>&cus=<?php echo $_GET['cus'];?>"   title="Click to pay" >
+				  <button class="btn btn-success">Set Payment</button></a><?php } ?>
+					<a href="bill2.php?id=<?php echo $row2['invoice_number'];?>"   title="Click to pay" >
+				  <button class="btn btn-primary">View</button></a>
+					</td>
+					</tr>
+				<?php
+  //    }
+		} }
+				}
+
+			if($b_tot > 1){
+			?>
+					   <tr   class=" bg-gray"   >
+						   <td><?php echo $cus;?></td>
+
+				<td >Total</td>
+  <td></td>
+
+
+			<td></td><td></td>
+		<td><span class="pull-right badge bg-red"><?php echo $b_tot; ?></span></td>
+
+	<td></td><td></td><td></td>
+                </tr>
+	<?php
+//$tot+=$b_tot;
+} } ?>
+
+
+
+                </tbody>
+
+                <tfoot   class=" bg-aqua"   >
+
+				<td  colspan="3" >Total</td>
+
+
+			<td></td><td></td>
+		<td><span class="pull-right badge bg-muted"><?php echo number_format($tot,1); ?></span></td>
+
+	<td></td><td></td><td></td>
+                </tfoot>
+              </table>
 
 
 
