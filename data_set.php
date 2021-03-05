336c33ecebba4d5a5c0003e05fea093ec75214ae
erand-palliyaguru <erandasampath2000@gmail.com>
2021-03-05 09:42:40 +0530

    up

--- data_set.php
+++ data_set.php
@@ -3,18 +3,18 @@ session_start();
 include('connect.php');
 date_default_timezone_set("Asia/Colombo");
 
-$result = $db->prepare("SELECT * FROM sales_list WHERE product_id='3'  AND  action='0' AND date BETWEEN '2021-01-01' and '2021-01-10' ");
+$result = $db->prepare("SELECT * FROM special_price WHERE product_id='3'  AND  price='6295' ");
 $result->bindParam(':userid', $invo);
 $result->execute();
 for($i=0; $row = $result->fetch(); $i++){
 
-$id=$row['id'];
-$date=$row['date'];
 
-$pid='1';
-                        $sql = "UPDATE sales_list
-                        SET price_id=?
-                     		WHERE id=? ";
+$id=$row['customer_id'];
+
+$pid='0';
+                        $sql = "UPDATE customer
+                        SET price_37=?
+                     		WHERE customer_id=? ";
                       $q = $db->prepare($sql);
                       $q->execute(array($pid,$id));
 
