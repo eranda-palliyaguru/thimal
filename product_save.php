ed2541400fbc1424792ddc8b8f48446e7e1cb7d9
erand-palliyaguru <erandasampath2000@gmail.com>
2021-04-23 09:29:12 +0530

    up

--- product_save.php
+++ product_save.php
@@ -10,9 +10,9 @@ $c = $_POST['o_price'];
 
 // query
 
-$sql = "INSERT INTO products (gen_name,price,price,o_price) VALUES (:a,:b,:c,:d)";
+$sql = "INSERT INTO products (gen_name,price,o_price) VALUES (:a,:b,:d)";
 $q = $db->prepare($sql);
-$q->execute(array(':a'=>$a,':b'=>$b,':c'=>$b,':d'=>$c));
+$q->execute(array(':a'=>$a,':b'=>$b,':d'=>$c));
 
 header("location: product.php");
 
