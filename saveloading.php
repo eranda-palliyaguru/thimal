a8c09d17a3c50e4b75639a1f3fcb044fc2fbd692
erand-palliyaguru <erandasampath2000@gmail.com>
2021-06-10 00:38:31 +0530

    update 18L emty loading error

--- saveloading.php
+++ saveloading.php
@@ -53,7 +53,7 @@ $empty=$row['product_name'];
 
 $act='load';
 
-$sql = "UPDATE lorry 
+$sql = "UPDATE lorry
         SET action=?
 		WHERE lorry_no=?";
 $q = $db->prepare($sql);
@@ -67,8 +67,8 @@ $q->execute(array($act,$c));
 
 if($yard==1){
 	$yard1='Main Yard';
-	
-$sql = "UPDATE products 
+
+$sql = "UPDATE products
         SET qty=qty-?
 		WHERE product_id=?";
 $q = $db->prepare($sql);
@@ -89,15 +89,29 @@ $lob=$row['qty'];
 }
 
 
-
+$result = $db->prepare("SELECT * FROM loading_list WHERE product_code='$b' and loading_id='$id'");
+$result->bindParam(':userid', $b);
+$result->execute();
+for($i=0; $row = $result->fetch(); $i++){
+$loading_list_id=$row['transaction_id'];
+}
 
 
 
 $g=$asasa;
 // query
-$sql = "INSERT INTO loading_list (product_code,qty,price,lorry_no,profit,product_name,date,action,qty_sold,loading_time,loading_id,load_yard_before) VALUES (:b,:f,:g,:c,:h,:i,:j,:k,:l,:j1,:m,:lb)";
-$q = $db->prepare($sql);
-$q->execute(array(':b'=>$b,':c'=>$c,':f'=>$f,':g'=>$g,':h'=>$h,':i'=>$ii,':j'=>$j,':k'=>$k,':l'=>$l,':j1'=>$j1,':m'=>$id,':lb'=>$lob));
+if (isset($loading_list_id)) {
+	$sql = "UPDATE loading_list
+	        SET qty=qty+?
+			WHERE transaction_id=?";
+	$q = $db->prepare($sql);
+	$q->execute(array($f,$loading_list_id));
+}else {
+	$sql = "INSERT INTO loading_list (product_code,qty,price,lorry_no,profit,product_name,date,action,qty_sold,loading_time,loading_id,load_yard_before) VALUES (:b,:f,:g,:c,:h,:i,:j,:k,:l,:j1,:m,:lb)";
+	$q = $db->prepare($sql);
+	$q->execute(array(':b'=>$b,':c'=>$c,':f'=>$f,':g'=>$g,':h'=>$h,':i'=>$ii,':j'=>$j,':k'=>$k,':l'=>$l,':j1'=>$j1,':m'=>$id,':lb'=>$lob));
+
+}
 
 
 $action=1;
@@ -105,17 +119,12 @@ $sql = "INSERT INTO stock_log (product_id,qty,product_name,date,time,action,sour
 $q = $db->prepare($sql);
 $q->execute(array(':b'=>$b,':f'=>$f,':i'=>$ii,':j'=>$j,':k'=>$action,':m'=>$id,':lb'=>$lob,':ti'=>$time,':ty'=>$type,':us'=>$user_id));
 
+
+
 if($empty>=1){
-	
-
-
-
-
 //###################################     edit qty      #################################
-
-
 if($yard==1){
-$sql = "UPDATE products 
+$sql = "UPDATE products
         SET qty=qty-?
 		WHERE product_id=?";
 $q = $db->prepare($sql);
@@ -123,9 +132,9 @@ $q->execute(array($f,$empty));
 }
 
 //##########################################################################################
-	
 
-	
+
+
 $result = $db->prepare("SELECT * FROM products WHERE product_id= :userid");
 $result->bindParam(':userid', $empty);
 $result->execute();
@@ -135,14 +144,30 @@ $h=$row['profit'];
 $ii=$row['gen_name'];
 $lob=$row['qty'];
 }
-	
-	
+
+
+$result = $db->prepare("SELECT * FROM loading_list WHERE product_code='$empty' and loading_id='$id'");
+$result->bindParam(':userid', $b);
+$result->execute();
+for($i=0; $row = $result->fetch(); $i++){
+$loading_list_id_e=$row['transaction_id'];
+}
+
+
+if (isset($loading_list_id_e)) {
+	$sql = "UPDATE loading_list
+	        SET qty=qty+?
+			WHERE transaction_id=?";
+	$q = $db->prepare($sql);
+	$q->execute(array($f,$loading_list_id_e));
+}else {
+
 $sql = "INSERT INTO loading_list (product_code,qty,price,lorry_no,profit,product_name,date,action,qty_sold,loading_id,load_yard_before) VALUES (:b,:f,:g,:c,:h,:i,:j,:k,:l,:m,:lb)";
 $q = $db->prepare($sql);
 $q->execute(array(':b'=>$empty,':c'=>$c,':f'=>$f,':g'=>$g,':h'=>$h,':i'=>$ii,':j'=>$j,':k'=>$k,':l'=>$l,':m'=>$id,':lb'=>$lob));
+}
 
-	
-	
+
 $action=1;
 $sql = "INSERT INTO stock_log (product_id,qty,product_name,date,time,action,source_id,yard_qty,type,user_id) VALUES (:b,:f,:i,:j,:ti,:k,:m,:lb,:ty,:us)";
 $q = $db->prepare($sql);
@@ -150,7 +175,7 @@ $q->execute(array(':b'=>$empty,':f'=>$f,':i'=>$ii,':j'=>$j,':k'=>$action,':m'=>$
 }
 
 
-	
+
 
 
 header("location: loading2.php?id=$id");
