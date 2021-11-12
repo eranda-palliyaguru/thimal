fd2929ab1e7656f09d1cef94d167eba1c8d7add6
Eranda Palliyaguru <74122774+eranda-palliyaguru@users.noreply.github.com>
2021-11-12 09:55:25 +0530

    Update rep_save.php
    employee save

--- rep_save.php
+++ rep_save.php
@@ -1,19 +1,30 @@
 <?php
 session_start();
 include('connect.php');
-$a = $_POST['rep_name'];
-$b = $_POST['address'];
-$c = $_POST['content'];
+$name = $_POST['rep_name'];
+$type = $_POST['type'];
+$contect = $_POST['content'];
+
+$user = $_POST['user'];
+$password = $_POST['password'];
 
 
 
+$sql = "INSERT INTO employee (name,phone_no,type,action) VALUES (:a,:b,:c,:d)";
+$q = $db->prepare($sql);
+$q->execute(array(':a'=>$name,':b'=>$contect,':c'=>$type,':d'=>"1"));
 
+   $result = $db->prepare("SELECT * FROM employee WHERE action='1' AND name='$name'  ORDER by id DESC limit 1  ");
+   $result->bindParam(':userid', $date);
+   $result->execute();
+   for($i=0; $row = $result->fetch(); $i++){ $emp=$row['id']; }
 
 
 // query
-$sql = "INSERT INTO rep (rep_name,rep_address,contact_no) VALUES (:a,:b,:c)";
+$sql = "INSERT INTO user (username,password,name,position,employeeid) VALUES (?,?,?,?,?)";
 $q = $db->prepare($sql);
-$q->execute(array(':a'=>$a,':b'=>$b,':c'=>$c));
+$q->execute(array($user,$password,$name,"lorry",$emp));
+
 header("location: rep.php");
 
 
