3444748646f3028493c4e5dd4ace5568e3bad46c
Eranda Palliyaguru <74122774+eranda-palliyaguru@users.noreply.github.com>
2022-03-21 12:20:47 +0530

    v2

--- data_set.php
+++ data_set.php
@@ -7,17 +7,24 @@ date_default_timezone_set("Asia/Colombo");
 $resultz = $db->prepare("SELECT * FROM payment  WHERE type='credit' AND date BETWEEN '2022-03-08' AND '2022-03-21' ");
 $resultz->bindParam(':userid', $inva);
 $resultz->execute();
+for($i=0; $row1 = $resultz->fetch(); $i++){
+$cus_id=$row1['customer_id'];
+
+ 
+$result = $db->prepare("SELECT * FROM customer  WHERE customer_id='$cus_id' ");
+$result->bindParam(':userid', $inva);
+$result->execute();
 for($i=0; $row = $resultz->fetch(); $i++){
 
 
-
-echo $row['transaction_id']."<br>";
+ 
+echo $row['credit_period']."_____".$row1['credit_period']."<br>";
  
 
  
 
 
-
+}
 }
 
 ?>
