f2efdd3b5fb14bcfcd8f8197fa5d7b91f1cec068
Eranda Palliyaguru <74122774+eranda-palliyaguru@users.noreply.github.com>
2022-03-21 10:53:14 +0530

    uup

--- data_set.php
+++ data_set.php
@@ -4,31 +4,15 @@ include('connect.php');
 date_default_timezone_set("Asia/Colombo");
 
 
-$resultz = $db->prepare("SELECT * FROM payment WHERE type='chq' AND chq_action='3' ");
+$resultz = $db->prepare("SELECT p.transaction_id,p.date,p.customer_id,c.customer_id,c.credit_period,p.credit_period FROM payment AS p INNER JOIN customer AS c ON p.customer_id=c.customer_id WHERE p.type='credit' AND p.date BETWEEN '2022-03-08' AND '2022-03-21' ");
 $resultz->bindParam(':userid', $inva);
 $resultz->execute();
 for($i=0; $row = $resultz->fetch(); $i++){
 $id=$row['transaction_id'];
 
 
-$result = $db->prepare("SELECT * FROM credit_payment WHERE pay_id='$id' ");
-$result->bindParam(':userid', $inva);
-$result->execute();
-for($i=0; $row1 = $result->fetch(); $i++){
-
-  $pay_amount=$row1['pay_amount'];
-  $sales_id=$row1['sales_id'];
-
-  
-  $resultz2 = $db->prepare("SELECT * FROM payment WHERE sales_id='$sales_id' AND type='credit' AND memo='CHQ RETURN' ");
-  $resultz2->bindParam(':userid', $inva);
-  $resultz2->execute();
-  for($i=0; $row2 = $resultz2->fetch(); $i++){
-    $amount=$row2['amount'];
-    if($pay_amount < $amount){ echo $sales_id."<br>";  }
-  }
-
- }
+echo $row['p.credit_period']."_____".$row['c.credit_period']."<br>";
+ 
 
 
 
