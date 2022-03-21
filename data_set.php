d5b468c91a4f82fcc2e491651a4b16d0634cba61
Eranda Palliyaguru <74122774+eranda-palliyaguru@users.noreply.github.com>
2022-03-21 12:10:12 +0530

    v1.4

--- data_set.php
+++ data_set.php
@@ -4,14 +4,14 @@ include('connect.php');
 date_default_timezone_set("Asia/Colombo");
 
 
-$resultz = $db->prepare("SELECT p.transaction_id,p.date,p.customer_id,c.customer_id,c.credit_period,p.credit_period FROM payment AS p INNER JOIN customer AS c ON p.customer_id=c.customer_id WHERE p.type='credit' AND p.date BETWEEN '2022-03-08' AND '2022-03-21' ");
+$resultz = $db->prepare("SELECT * FROM payment  WHERE type='credit' AND date BETWEEN '2022-03-08' AND '2022-03-21' ");
 $resultz->bindParam(':userid', $inva);
 $resultz->execute();
 for($i=0; $row = $resultz->fetch(); $i++){
 
 
 
-echo $row['p.transaction_id']."<br>";
+echo $row['transaction_id']."<br>";
  
 
  
