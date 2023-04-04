cf9d3fcc4864fde41a06254409388e554a40f69e
eranda-palliyaguru <erandasampath2000@gmail.com>
2023-04-04 12:21:18 +0530

    test up

--- index.php
+++ index.php
@@ -554,23 +554,6 @@ $ba=$rowz['amount'];
 
 
 
-
-
-
-            <div class="col-lg-3 col-xs-6">
-                <!-- small box -->
-                <div class="small-box bg-aqua">
-                    <div class="inner">
-                        <h3><?php echo $ba; ?></h3>
-
-                        <p>Petty Cash BOX</p>
-                    </div>
-                    <div class="icon">
-                        <i class="ion ion-group"></i>
-                    </div>
-                    <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-                </div>
-            </div>
             <!-- ./col -->
     </div>
     <?php
