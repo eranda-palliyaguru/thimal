7acc2f61f4b9177030b9130b84f874295789690d
Eranda Palliyaguru <74122774+eranda-palliyaguru@users.noreply.github.com>
2024-02-16 14:06:33 +0530

    api test 1

---
+++ api/v2/login.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+include('../../connect.php');
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+//echo $r_data[0];
+$user=$_POST['user'];
+$password=sha1($_POST['password']);
+
+
+
+$action="not";
+$name='';
+$user_id=0;
+
+$result = $db->prepare("SELECT * FROM user WHERE username='$user' AND password='$password' ");
+$result->bindParam(':userid', $res);
+$result->execute();
+for($i=0; $row = $result->fetch(); $i++){ 
+    $user_id=$row['id'];
+    $name=$row['name'];
+ }
+ 
+
+ $key=sha1(date('syihadm'));
+
+
+    $action="ok";
+
+
+
+$result_array = array ("action" => $action,
+                       "user_id" => $user_id,
+                       "name" => $name,
+                       "key" => $key);
+
+
+echo (json_encode ( $result_array ));
+
+?>
