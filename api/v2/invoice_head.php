7b73ec6b4e0505e480eaa2344a8bd135f96cf813
Eranda Palliyaguru <74122774+eranda-palliyaguru@users.noreply.github.com>
2024-02-16 16:26:39 +0530

    update

---
+++ api/v2/invoice_head.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+include('../../connect.php');
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+//echo $r_data[0];
+$user=$_POST['user_id'];
+
+
+
+
+
+$result = $db->prepare("SELECT * FROM sales WHERE cashier='$user' ORDER by transaction_id DESC limit 0,1 ");
+$result->bindParam(':userid', $res);
+$result->execute();
+for($i=0; $row = $result->fetch(); $i++){ 
+    $result_array = array ("invoice_no" => $row['invoice_number'],
+    "name" => $row['name'],
+    "date" => $row['date'],
+    "action" => 'ok',
+    "lorry_no" => $row['lorry_no']);
+ }
+ 
+
+ 
+
+
+
+
+
+echo (json_encode ( $result_array ));
+
+?>
