b0f91bac3519719745e979726aafb913a3a30844
Eranda Palliyaguru <74122774+eranda-palliyaguru@users.noreply.github.com>
2024-02-26 14:05:11 +0530

    q up

--- loading_view.php
+++ loading_view.php
@@ -390,21 +390,27 @@ for ($i = 0; $row = $result->fetch(); $i++) {
                                 <tr>
                                     <td colspan="2">Total</td>
 
+                                    
+                                    <td> <span class="pull-right badge bg-yellow"> <?php echo $total['5']; ?> </span>
+                                    </td>
                                     <td> <span class="pull-right badge bg-muted"> <?php echo $total['1']; ?> </span>
                                     </td>
-                                    <td> <span class="pull-right badge bg-yellow"> <?php echo $total['5']; ?> </span>
+
+                                    
+                                    <td> <span class="pull-right badge bg-yellow"> <?php echo $total['6']; ?> </span>
                                     </td>
                                     <td> <span class="pull-right badge bg-muted"> <?php echo $total['2']; ?> </span>
                                     </td>
-                                    <td> <span class="pull-right badge bg-yellow"> <?php echo $total['6']; ?> </span>
+
+                                    
+                                    <td> <span class="pull-right badge bg-yellow"> <?php echo $total['7']; ?> </span>
                                     </td>
                                     <td> <span class="pull-right badge bg-muted"> <?php echo $total['3']; ?> </span>
                                     </td>
-                                    <td> <span class="pull-right badge bg-yellow"> <?php echo $total['7']; ?> </span>
+                                    
+                                    <td> <span class="pull-right badge bg-yellow"> <?php echo $total['8']; ?> </span>
                                     </td>
                                     <td> <span class="pull-right badge bg-muted"> <?php echo $total['4']; ?> </span>
-                                    </td>
-                                    <td> <span class="pull-right badge bg-yellow"> <?php echo $total['8']; ?> </span>
                                     </td>
 
                                     <?php
