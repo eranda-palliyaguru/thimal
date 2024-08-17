a888b04711174e56db2645b981b0b2aca52107a8
Eranda Palliyaguru <74122774+eranda-palliyaguru@users.noreply.github.com>
2024-08-17 20:27:26 +0530

    update

---
+++ api/v2/gps_test.php
@@ -0,0 +1,24 @@
+<?php 
+session_start();
+include('../../connect.php');
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Display all POST parameters
+    foreach ($_POST as $key => $value) {
+        $text.= "Parameter Name: " . htmlspecialchars($key) . "\n";
+        $text.= "Parameter Value: " . htmlspecialchars($value) . "\n\n";
+    }
+} else {
+    $text= "No POST data received.";
+}
+
+echo $text;
+
+$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+
+fwrite($myfile, $text);
+fclose($myfile);
