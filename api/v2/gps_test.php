24b58d5557fa86ad511305642dd2996f7f500293
Eranda Palliyaguru <74122774+eranda-palliyaguru@users.noreply.github.com>
2024-08-17 20:29:46 +0530

    update

--- api/v2/gps_test.php
+++ api/v2/gps_test.php
@@ -6,6 +6,7 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: GET");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
+$text='';
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Display all POST parameters
     foreach ($_POST as $key => $value) {
